b26a431c8ba1dbe8e7071e229810fc22aa44dd5e
Nikhil Rane <nikhilrane1992@gamil.com>
2016-07-31 01:55:24 +0530

    added database path in project

--- ReactJSTutorialWithDjango/ReactJSTutorialWithDjango/settings.py
+++ ReactJSTutorialWithDjango/ReactJSTutorialWithDjango/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'ReactJSTutorialWithDjango.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../Project_Database/ReactJSTutorialWithDjango'),
     }
 }
 
